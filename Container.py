e53173fc01cef27e52ce68c3a88b158afb64412b
0916174 <0916174@hr.nl>
2017-11-30 17:53:55 +0100

    Added existing code and Main file.

---
+++ Container.py
@@ -0,0 +1,34 @@
+import random
+
+def GetNumbers():
+	#Maak variabele om later te returnen
+	result = []
+	
+	#Maak variabele om alle nummers die we nodig hebben in te bewaren
+	allnumbers = []
+	
+	#Stop de nummers 1 t/m 5 er 6 keer in
+	for i in range(6):
+		allnumbers.extend(range(1,6))
+	
+	#Shuffle 'em around
+	random.shuffle(allnumbers)
+
+	#Doe 5 keer...
+	for i in range(0,30,6):
+		#Maak een tijdelijke variabele waar 1 blok met getallen in gaat
+		part = []
+
+		#Stop de volgende 6 getallen van 'allnumbers' in 'part'
+		#
+		#(De eerste keer dat dit runt, wordt 'i' dus [0,1,2,3,4,5], dus de eerste 6 getallen
+		# uit 'allnumbers' worden in een array/blok gestopt en dat wordt aan 'result' toegevoegd.
+		# De 2e keer worden de getallen met de index [6,7,8,9,10,11] uit 'allnumbers' als blok toegevoegd aan 'result', enz.)
+		for j in range(i,i+6):
+			part.append(allnumbers[j])
+
+		#Voeg dit blok toe aan het resultaat
+		result.append(part)
+
+	#Return het resultaat.
+	return result
---
+++ Main.py
@@ -0,0 +1,5 @@
+import pygame
+import Container
+
+print(Container.GetNumbers())
+print(r"Check deze tutorial: https://www.youtube.com/watch?v=ujOTNg17LjI")
